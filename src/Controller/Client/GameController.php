aec82ea991621bf9e8cef6bd43ffef3ca78ec075
Novali28 <nathanael.v@outlook.fr>
2020-12-22 00:09:58 +0100

    infos et download du jeu

---
+++ src/Controller/Client/GameController.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace App\Controller\Client;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GameController extends AbstractController
+{
+    /**
+     * @Route("/infos", name="infos")
+     */
+    public function infos(Request $request)
+    {
+        return $this->render('infos.html.twig');
+    }
+
+    /**
+     * @Route("/dowload", name="download", methods={"GET"})
+     */
+    public function download(Request $request)
+    {
+        $exePath = $this->getParameter('download_dir').'/test(7).exe';
+        return $this->file($exePath);
+    }
+}
